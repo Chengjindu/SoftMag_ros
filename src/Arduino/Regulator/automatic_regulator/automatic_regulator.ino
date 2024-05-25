e906e2f4674bd967959814506235234a547ae553
Chengjin <xiaoniao.chengjin@gmail.com>
2024-05-25 22:36:02 +0200

    Adding testing mode

---
+++ src/Arduino/Regulator/automatic_regulator/automatic_regulator.ino
@@ -0,0 +1,161 @@
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+#include <ros.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/String.h>
+#include <ArduinoJson.h>
+
+Adafruit_MCP4725 dac;
+ros::NodeHandle nh; 
+int monitorPin = A0; 
+
+float desiredPressure = 0.0; 
+float maxPressure = 35.0;
+bool motorTrigger = false;
+bool forceClosureTrigger = false;
+
+std_msgs::String graspStableMsg;
+std_msgs::String releaseFinishMsg;
+std_msgs::Float32 pressure_msg;
+ros::Publisher graspStablePub("grasp_stable", &graspStableMsg);
+ros::Publisher releaseFinishPub("release_finish", &releaseFinishMsg);
+ros::Publisher pressurereadingPub("pressure_reading", &pressure_msg);
+
+void motorStopCallback(const std_msgs::String& msg) {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, msg.data);
+  bool motor_stop = doc["motor_stop"];
+  motorTrigger = motor_stop;
+}
+
+void forceClosureCallback(const std_msgs::String& msg) {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, msg.data);
+
+  bool force_closure = doc["force_closure"];
+  
+  if (force_closure && !forceClosureTrigger) {
+    if (desiredPressure + 1.0 <= maxPressure) {
+      desiredPressure += 0.5;
+      forceClosureTrigger = true;
+    } else {
+      forceClosureTrigger = true;
+    }
+    motorTrigger = false;
+
+    StaticJsonDocument<200> stableDoc;
+    stableDoc["grasp_stable_flag"] = true;
+    char jsonBuffer[256];
+    serializeJson(stableDoc, jsonBuffer);
+    graspStableMsg.data = jsonBuffer;
+    graspStablePub.publish(&graspStableMsg);
+  }
+}
+
+void releaseCallback(const std_msgs::String& msg) {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, msg.data);
+  bool release_flag = doc["release_flag"];
+
+  if (release_flag) {
+    desiredPressure = 0.0;
+  }
+
+  StaticJsonDocument<200> stableDoc;
+  stableDoc["release_finish_flag"] = true;
+  char jsonBuffer[256];
+  serializeJson(stableDoc, jsonBuffer);
+  releaseFinishMsg.data = jsonBuffer;
+  releaseFinishPub.publish(&releaseFinishMsg);
+
+  stableDoc["grasp_stable_flag"] = false;
+  serializeJson(stableDoc, jsonBuffer);
+  graspStableMsg.data = jsonBuffer;
+  graspStablePub.publish(&graspStableMsg);
+}
+
+void restartCallback(const std_msgs::String& msg) {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, msg.data);
+
+  bool restart_flag = doc["restart_flag"];
+  if (restart_flag) {
+    motorTrigger = false;
+    forceClosureTrigger = false;
+  }
+}
+
+void stopallCallback(const std_msgs::String& msg) {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, msg.data);
+
+  bool stop_all_flag = doc["stop_all_flag"];
+  if (stop_all_flag) {
+    desiredPressure = 0.0;
+    motorTrigger = false;
+    forceClosureTrigger = false;
+
+    StaticJsonDocument<200> stableDoc;
+    char jsonBuffer[256];
+    stableDoc["grasp_stable_flag"] = false;
+    serializeJson(stableDoc, jsonBuffer);
+    graspStableMsg.data = jsonBuffer;
+    graspStablePub.publish(&graspStableMsg);
+  }
+}
+
+ros::Subscriber<std_msgs::String> motorStopSub("motor_stop", &motorStopCallback);
+ros::Subscriber<std_msgs::String> forceClosureSub("force_closure", &forceClosureCallback);
+ros::Subscriber<std_msgs::String> releaseSub("release", &releaseCallback);
+ros::Subscriber<std_msgs::String> restartSub("restart", &restartCallback);
+ros::Subscriber<std_msgs::String> stopallSub("stop_all", &stopallCallback);
+
+void setup() {
+  Serial.begin(57600);
+  dac.begin(0x60);
+  nh.initNode();
+  nh.subscribe(motorStopSub);
+  nh.subscribe(forceClosureSub);
+  nh.subscribe(releaseSub);
+  nh.subscribe(restartSub);
+  nh.subscribe(stopallSub);
+  nh.advertise(pressurereadingPub);
+  nh.advertise(graspStablePub);
+  nh.advertise(releaseFinishPub);
+}
+
+void loop() {
+
+  uint16_t dacValue = (uint16_t)((desiredPressure / 50.0) * 4095);
+  dac.setVoltage(dacValue, false);
+
+  if (motorTrigger && desiredPressure < maxPressure) {
+    desiredPressure += 1.0; 
+    if (desiredPressure >= maxPressure) {
+      desiredPressure = maxPressure;
+      motorTrigger = false;
+      forceClosureTrigger = true;
+      
+      StaticJsonDocument<200> stableDoc;
+      stableDoc["grasp_stable_flag"] = true;
+      char jsonBuffer[256];
+      serializeJson(stableDoc, jsonBuffer);
+      graspStableMsg.data = jsonBuffer;
+      graspStablePub.publish(&graspStableMsg);
+    }
+    uint16_t dacValue = (uint16_t)((desiredPressure / 50.0) * 4095);
+    dac.setVoltage(dacValue, false);
+    delay(100);
+  }
+
+  float sensorValue = analogRead(monitorPin); 
+  float voltage = sensorValue * (5.0 / 1023.0); 
+  float pressure = ((voltage - 1) / 4.0) * 100.0; 
+
+  pressure_msg.data = pressure;
+  pressurereadingPub.publish(&pressure_msg);
+
+  nh.spinOnce();
+  delay(100);
+}
+
--- src/sofa_simulation/src/sofa_simulation_node.py
+++ src/sofa_simulation/src/sofa_simulation_node.py
@@ -1,40 +1,226 @@
 #!/usr/bin/env python3
+# coding: utf8
 
 import os
 import rospy
-from std_msgs.msg import String  # Import ROS standard message types
+from std_msgs.msg import Float32MultiArray  # wrapper for ROS primitive types, see : https://github.com/ros2/common_interfaces/tree/master/std_msgs
+
 import Sofa
 import Sofa.Gui
 import SofaRuntime
+import math
+from controller import WholeGripperController
+# from splib3.loaders import loadPointListFromFile
+import sofaros
 
-def createScene(root):
-    root.addObject('RequiredPlugin', name='Sofa.Component.StateContainer')
-    root.addObject('DefaultAnimationLoop')
-    root.addObject('RequiredPlugin', name='SofaOpenglVisual')
+import signal
+import sys
 
-    node1 = root.addChild("Node1")
-    node2 = root.addChild("Node2")
-    node1.addObject("MechanicalObject", template="Rigid3d", position="0 0 0   0 0 0 1", showObject="1")
-    node2.addObject("MechanicalObject", template="Rigid3d", position="1 1 1   0 0 0 1", showObject="1")
+
+# Get the directory of your script
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+
+# Fingers information
+youngModulusFingers = 70 #kPa
+youngModulusFabric = 7500000 #kPa
+youngModulusFoam = 130 #kPa
+poissonRatioFingers = 0.4
+poissonRatioFabric = 0.2
+poissonRatioFoam = 0.4
+foamthickness = '8' #millimeters
+fingerMass = 0.030 #kg
+fabricMass = 0.005 #kg
+foamMass = 0.005 #kg
+translateFinger1 = [0, 0, 0]
+translateFinger2 = [0, 0, -75]
+translations = [translateFinger1, translateFinger2]
+rotationFinger1 = [0, 0, -90]
+rotationFinger2 = [0, 180, -90]
+rotations = [rotationFinger1, rotationFinger2]
+
+
+def createScene(rootNode):
+    rootNode.addObject('RequiredPlugin',
+                       pluginName='SoftRobots SofaPython3 SofaLoader SofaSimpleFem SofaEngine SofaDeformable SofaImplicitOdeSolver SofaConstraint SofaSparseSolver SofaMeshCollision SofaRigid SofaOpenglVisual Sofa.Component.Collision.Detection.Algorithm')
+    rootNode.addObject('VisualStyle',
+                       displayFlags='showVisualModels hideBehaviorModels hideCollisionModels hideBoundingCollisionModels hideForceFields showInteractionForceFields hideWireframe')
+    rootNode.dt = 0.01
+    rootNode.gravity.value = [-9810, 0, 0]
+    rootNode.addObject('FreeMotionAnimationLoop')
+    rootNode.addObject('GenericConstraintSolver', tolerance=1e-12, maxIterations=10000)
+    rootNode.addObject('DefaultPipeline')
+    rootNode.addObject('BruteForceBroadPhase')
+    rootNode.addObject('BVHNarrowPhase')
+    rootNode.addObject('DefaultContactManager', response='FrictionContactConstraint', responseParams='mu=0.99')
+    rootNode.addObject('LocalMinDistance', name='Proximity', alarmDistance=10, contactDistance=1, angleCone=0.0)
+
+    rootNode.addObject('BackgroundSetting', color=[0.2, 0.22, 0.25, 1.])
+    rootNode.addObject('OglSceneFrame', style='Arrows', alignment='TopRight')
+    
+    for i in range(2):
+        ####################################sofaros.init()######
+        # Finger Model	 						 #
+        ##########################################
+        finger = rootNode.addChild('finger' + str(i + 1))
+        finger.addObject('EulerImplicitSolver', name='odesolver', rayleighStiffness=0.1, rayleighMass=0.1)
+        finger.addObject('SparseLDLSolver', name='preconditioner')
+
+        ##########################################
+        # Mechanics             				  #
+        ##########################################
+        # Use it to construct the absolute path to the mesh file
+        finger_mesh_file_path = os.path.join(script_directory, 'data', 'mesh', 'border8.msh')
+        finger.addObject('MeshGmshLoader', name='loader', filename=finger_mesh_file_path, rotation=rotations[i], translation=translations[i])
+        finger.addObject('MeshTopology', src='@loader', name='container')
+        finger.addObject('MechanicalObject', name='tetras', template='Vec3d')
+        finger.addObject('UniformMass', totalMass=fingerMass)
+        finger.addObject('TetrahedronFEMForceField', template='Vec3d', name='FEM', method='large', poissonRatio=poissonRatioFingers,
+                         youngModulus=youngModulusFingers)
+        finger.addObject('LinearSolverConstraintCorrection', solverName='preconditioner')
+
+        if (i+1) == 1:
+            ff = rootNode.finger1.loader.position
+        else:
+            ff = rootNode.finger2.loader.position
+        xmin = min(ff[:,0])
+        xmax = max(ff[:,0])
+        ymin = min(ff[:,1])
+        ymax = max(ff[:,1])  
+        zmin = min(ff[:,2])
+        zmax = max(ff[:,2])
+        
+        finger.addObject('BoxROI', name='boxROI', box=[xmin, ymin, zmin, xmin+10, ymax, zmax], drawBoxes=False, doUpdate=False)
+        finger.addObject('RestShapeSpringsForceField', points='@boxROI.indices', stiffness=1e12, angularStiffness=1e12)
+
+        
+        ##########################################
+        # Fabric Sub topology			   #
+        ########################################## 
+        fabric = finger.addChild('fabric' + str(i + 1))
+        fabric_mesh_file_path = os.path.join(script_directory, 'data', 'mesh', 'fabric2.msh')
+        fabric.addObject('MeshGmshLoader', name='loader', filename=fabric_mesh_file_path, rotation=rotations[i], translation=translations[i])
+        fabric.addObject('MeshTopology', src='@loader', name='topocontainer')
+        fabric.addObject('MechanicalObject', name='tetras', template='Vec3d')
+        fabric.addObject('UniformMass', totalMass=fabricMass)
+        fabric.addObject('TetrahedronFEMForceField', template='Vec3d', name='FEM', method='large', poissonRatio=poissonRatioFabric,
+                        youngModulus=youngModulusFabric)
+        fabric.addObject('BarycentricMapping', name='mapping', mapForces=False, mapMasses=False)
+
+        ##########################################
+        # Foam Sub topology			   #
+        ##########################################
+        foam = finger.addChild('foam' + str(i + 1))
+        foam_mesh_file_path = os.path.join(script_directory, 'data', 'mesh', 'foam'+foamthickness+'.msh')
+        foam.addObject('MeshGmshLoader', name='loader', filename=foam_mesh_file_path, rotation=rotations[i], translation=translations[i])
+        foam.addObject('MeshTopology', src='@loader', name='container')
+        foam.addObject('MechanicalObject', name='tetras', template='Vec3d')
+        foam.addObject('UniformMass', totalMass=foamMass)
+        foam.addObject('TetrahedronFEMForceField', template='Vec3d', name='FEM', method='large', poissonRatio=poissonRatioFoam,
+                         youngModulus=youngModulusFoam)
+        foam.addObject('BarycentricMapping', name='mapping', mapForces=False, mapMasses=False)
+        
+        ##########################################
+        # Cavity Constraint						 #
+        ##########################################
+        cavity = finger.addChild('cavity')
+        cavity_file_path = os.path.join(script_directory, 'data', 'mesh', 'tchambers.stl')
+        cavity.addObject('MeshSTLLoader', name='loader', filename=cavity_file_path, translation=translations[i], rotation=rotations[i])
+        cavity.addObject('MeshTopology', src='@loader', name='topo')
+        cavity.addObject('MechanicalObject', name='cavity')
+        cavity.addObject('SurfacePressureConstraint', name='SurfacePressureConstraint', template='Vec3d', value=0.0001,
+                          triangles='@topo.triangles', valueType='pressure')
+        cavity.addObject('BarycentricMapping', name='mapping', mapForces=False, mapMasses=False)
+
+        ##########################################
+        # Collision							  #
+        ##########################################
+        collisionFinger = finger.addChild('collisionFinger')
+        collisionFinger_file_path = os.path.join(script_directory, 'data', 'mesh', 'border8.stl')
+        collisionFinger.addObject('MeshSTLLoader', name='loader', filename=collisionFinger_file_path, translation=translations[i], rotation=rotations[i])
+        collisionFinger.addObject('MeshTopology', src='@loader', name='topo')
+        collisionFinger.addObject('MechanicalObject', name='collisMech')
+        collisionFinger.addObject('TriangleCollisionModel', selfCollision=False)
+        collisionFinger.addObject('LineCollisionModel', selfCollision=False)
+        collisionFinger.addObject('PointCollisionModel', selfCollision=False)
+        collisionFinger.addObject('BarycentricMapping')
+
+        # collisionFabric = fabric.addChild('collisionFabric')
+        # collisionFabric.addObject('MeshSTLLoader', name='loader', filename='data/mesh/fabric2.stl', translation=translations[i], rotation=rotations[i])
+        # collisionFabric.addObject('MeshTopology', src='@loader', name='topo')
+        # collisionFabric.addObject('MechanicalObject', name='collisMech')
+        # collisionFabric.addObject('TriangleCollisionModel', selfCollision=False)
+        # collisionFabric.addObject('LineCollisionModel', selfCollision=False)
+        # collisionFabric.addObject('PointCollisionModel', selfCollision=False)
+        # collisionFabric.addObject('BarycentricMapping')
+
+        # collisionFoam = foam.addChild('collisionFoam')
+        # collisionFoam.addObject('MeshSTLLoader', name='loader', filename='data/mesh/foam8.stl', translation=translations[i], rotation=rotations[i])
+        # collisionFoam.addObject('MeshTopology', src='@loader', name='topo')
+        # collisionFoam.addObject('MechanicalObject', name='collisMech')
+        # collisionFoam.addObject('TriangleCollisionModel', selfCollision=False)
+        # collisionFoam.addObject('LineCollisionModel', selfCollision=False)
+        # collisionFoam.addObject('PointCollisionModel', selfCollision=False)
+        # collisionFoam.addObject('BarycentricMapping')
+
+        ##########################################
+        # Visualizations					  #
+        ##########################################
+        visuFinger = finger.addChild('visuFinger')
+        visuFinger_file_path = os.path.join(script_directory, 'data', 'mesh', 'conic.stl')
+        visuFinger.addObject('MeshSTLLoader', name='loader', filename=visuFinger_file_path, translation=translations[i], rotation=rotations[i])
+        visuFinger.addObject('OglModel', src='@loader', color=[0.7, 0.7, 0.8, 0.7])
+        visuFinger.addObject('BarycentricMapping')
+        
+        visuFabric = fabric.addChild('visuFabric')
+        visuFabric_file_path = os.path.join(script_directory, 'data', 'mesh', 'fabric2.stl')
+        visuFabric.addObject('MeshSTLLoader', name='loader', filename=visuFabric_file_path, translation=translations[i], rotation=rotations[i])
+        visuFabric.addObject('OglModel', src='@loader', color=[0.5, 0.3, 0.6, 0.95])
+        visuFabric.addObject('BarycentricMapping')
+        
+        visuFoam = foam.addChild('visuFoam')
+        visuFoam_file_path = os.path.join(script_directory, 'data', 'mesh', 'foam8.stl')
+        visuFoam.addObject('MeshSTLLoader', name='loader', filename=visuFoam_file_path, translation=translations[i], rotation=rotations[i])
+        visuFoam.addObject('OglModel', src='@loader', color=[0.9, 0.9, 0.4, 0.6])
+        visuFoam.addObject('BarycentricMapping')
+
+
+    rootNode.addObject(WholeGripperController(node=rootNode))
+
+    rosNode = sofaros.init("SofaNode")
+    rootNode.addObject(sofaros.RosReceiver(rosNode, Float32MultiArray, node=rootNode))
+
+    return rootNode
+
+def signal_handler(sig, frame):
+    rospy.loginfo("SIGINT received. Shutting down gracefully...")
+    Sofa.Gui.GUIManager.closeGUI()
+    rospy.signal_shutdown("SIGINT received")
+    sys.exit(0)
 
 def main():
     # Initialize the ROS node
-    rospy.init_node('sofa_simulation_node', anonymous=True)
+    # rospy.init_node('sofa_simulation_node', anonymous=True)
+
+    # Signal handling for graceful shutdown
+    signal.signal(signal.SIGINT, signal_handler)
 
     # Make sure to load all necessary libraries
     SofaRuntime.importPlugin("Sofa.Component.StateContainer")
 
+    # This now references the createScene method defined above.
     root = Sofa.Core.Node("root")   # Initialize the root node for the simulation.
     createScene(root)   # Set up the simulation scene.
 
-    Sofa.Simulation.init(root)  # Once defined, initialization of the scene graph
+    # Once defined, initialization of the scene graph
+    Sofa.Simulation.init(root)
 
     # Check supported GUIs
     supported_gui = Sofa.Gui.GUIManager.ListSupportedGUI(",")
-    print("Supported GUIs are: " + supported_gui)
+    rospy.loginfo("Supported GUIs are: " + supported_gui)
 
     # Initialize and run the GUI
-    Sofa.Gui.GUIManager.Init("simple_scene", "qt") # it can also be changed to "qglviewer"
+    Sofa.Gui.GUIManager.Init("simple_scene", "qglviewer") # Initialize with qglviewer or qt
     Sofa.Gui.GUIManager.createGUI(root, __file__)
     Sofa.Gui.GUIManager.MainLoop(root)
     Sofa.Gui.GUIManager.closeGUI()
@@ -47,4 +233,5 @@ def main():
     rospy.loginfo("SOFA simulation ended")
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
     main()
